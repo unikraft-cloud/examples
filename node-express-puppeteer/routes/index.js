ce9376255a16dd412b526cf094b37d824e40d5a3
Razvan Deaconescu <razvand@unikraft.io>
2024-06-12 18:32:34 +0300

    fix(examples): Update Puppeteer example
    Replace Puppeteer example with a service-based one. It provides a web
    interface, based on ExpressJS, to query the PDF generating functionality
    of Puppeteer.
    
    Add:
    
    * `Kraftfile`: build / run rules
    * `Dockerfile`: placeholder to extract the filesystem
    * `README.md`: document how to use
    * `.dockerignore` / `.gitignore`: ignore generated files
    * application files, extracted from https://github.com/christopher-talke/node-express-puppeteer-pdf-example
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ node-express-puppeteer/routes/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const crypto = require("crypto");
+const router = express.Router();
+const generatePdf = require("../controller/generatePdf");
+
+router.get("/", (_, res) => {
+  res.sendFile("index.html");
+});
+
+router.get("/generate-pdf", async (req, res) => {
+  let result;
+  try {
+    result = await generatePdf('url', req.query.url);
+  } catch (err) {
+    return res.status(500).send();
+  }
+  res.attachment(`node-express-puppeteer-pdf-example.pdf`);
+  res.contentType("application/pdf");
+  res.send(result);
+});
+
+router.post("/generate-pdf", async (req, res) => {
+  let result = await generatePdf('base64', req.body.content);
+  let filename = crypto.randomBytes(16).toString("hex").toUpperCase();
+  if (req.body.filename) {
+    let validated_file_name = req.body.filename.trim().replace(/[^a-zA-Z0-9_-]/gim,'');
+    filename = validated_file_name;
+  }
+  res.attachment(`${filename}.pdf`);
+  res.contentType("application/pdf");
+  res.send(result);
+});
+
+router.get("*", (req, res) => {
+  res.redirect("/");
+});
+
+module.exports = router;
