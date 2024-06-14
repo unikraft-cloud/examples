e5e17c96109c5ac38858de386ad62c80b32c3b56
Razvan Deaconescu <razvand@unikraft.io>
2024-06-14 23:48:08 +0300

    fix(ruby3.2-rails): Add CSRF protection
    This fixes a code scanning issue: https://github.com/kraftcloud/examples/security/code-scanning/1
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- ruby3.2-rails/app/controllers/hello_controller.rb
+++ ruby3.2-rails/app/controllers/hello_controller.rb
@@ -1,3 +1,7 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+end
+
 class HelloController < ApplicationController
   def index
     @text = "Hello, World!"
