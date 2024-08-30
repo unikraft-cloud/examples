03930fc7e8c4bac191b56c7b6e052ea39f43d1d7
Razvan Deaconescu <razvand@unikraft.io>
2024-08-30 11:29:38 +0300

    fix(flask-redis): Update files for Unikraft Cloud
    Update setup for using Unikraft Cloud (Compose):
    
    - Add `Kraftfile`.
    - Add `.dockerignore` to ignore `.unikraft/` directory.
    - Identify Redis host by environment variables in `app.py`.
    - Update `Dockerfile` and `compose.yaml` for Unikraft Cloud.
    - Add newline to printed message in `app.py`.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- flask-redis/app.py
+++ flask-redis/app.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from redis import Redis
+import os
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"])
 
 @app.route('/')
 def hello():
     redis.incr('hits')
     counter = str(redis.get('hits'),'utf-8')
-    return "This webpage has been viewed "+counter+" time(s)"
+    return "This webpage has been viewed "+counter+" time(s)\n"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
