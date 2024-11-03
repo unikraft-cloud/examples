65b5e04e466fb9fd03f057328a41e111ea53ff51
Razvan Deaconescu <razvand@unikraft.io>
2024-11-03 19:45:22 +0200

    spark/all-in-one: Use 172.16.3.1 as server address
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- apache-spark/all-in-one/wrapper.sh
+++ apache-spark/all-in-one/wrapper.sh
@@ -6,4 +6,4 @@ export JAVA_HOME=/opt/java/openjdk/
 hostname apache-spark
 export SPARK_NO_DAEMONIZE=1
 /opt/spark/sbin/start-master.sh &
-/opt/spark/sbin/start-worker.sh spark://localhost:7077
+/opt/spark/sbin/start-worker.sh spark://172.16.3.1:7077
